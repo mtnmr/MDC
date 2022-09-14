ef87486270dcf78ce91de9ab8dc6f8a535b765f3
mtnmr <mei.rila1013@gmail.com>
2022-09-14 16:15:59 +0900

    bottomSheet

--- BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
+++ BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bottomsheet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomSheetButton.setOnClickListener {
+            val bottomSheetFragment = BottomSheetFragment()
+            bottomSheetFragment.show(supportFragmentManager, BottomSheetFragment.TAG)
+        }
     }
 }
